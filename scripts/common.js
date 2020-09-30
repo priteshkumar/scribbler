84b3694def8613049606df1e4356453b6d93025a
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-09-30 20:42:00 +0530

    add signup modal

---
+++ scripts/common.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+    /*function handleSignUp(e){
+        e.preventDefault();
+        var name = document.getElementById("InputName").value;
+        var username = document.getElementById("InputUserName").value;
+        var password = document.getElementById("InputPassword").value;
+        var confirmPassword = document.getElementById("InputPassword2").value;
+        if(name == ""){
+            
+        }
+        else if(username == ""){
+
+        }
+        else if(password == ""){
+
+        }
+        else if(confirmPassword == ""){
+
+        }
+    }
+
+    var signupModalButton = document.getElementById('modalsignup');
+    signupModalButton.addEventListener('click',handleSignUp);*/
+
+};
