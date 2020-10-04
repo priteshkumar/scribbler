a4b6ae97eb2272ccf9ea3e9ecb29f31121477fd4
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-10-04 21:38:25 +0530

    fix display style for comments div

--- scripts/post.js
+++ scripts/post.js
@@ -2,19 +2,24 @@ var postList = [
     {
         "id": "card_1",
         "author": "Colby Fayock",
-        "subject":"What is linting and how can it save your time"
+        "subject": "What is linting and how can it save your time"
     }
 ];
 
-function initUI(){
+function initUI() {
     var postId = window.sessionStorage.getItem('postId');
     console.log(postId);
 }
 
 //initUI();
 
-function addComments(e){
-    var comment = document.getElementById('InputComment').value;
+function addComments(e) {
+    var inputComment = document.getElementById('InputComment');
+    var comment = inputComment.value;
     var comments = document.getElementById('comments');
+    if (comments.style.display == '') {
+        comments.style.display = 'block';
+    }
     comments.innerHTML += '<p>' + comment + '</p>';
+    inputComment.value = inputComment.defaultValue;
 }
