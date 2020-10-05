d768861ef6dc58c3132146c011f560e620340a0d
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-10-05 20:11:29 +0530

    add post details in post js

--- scripts/post.js
+++ scripts/post.js
@@ -1,27 +1,44 @@
 var postsMap = {
     "card_1": {
         "author": "Srishti Gupta",
-        "subject": "‘let’ me be a ‘const’(ant), not a ‘var’(iable)!",
-        "description": ""
+        "subject": "'let' me be a 'const'(ant), not a 'var'(iable)!",
+        "description": "Since JavaScript does not have any type-checking, either of these keywords can be used to declare" +
+            "variable of any type (datatype) in JavaScript.Though all the three keywords are used for the same" +
+            "purpose, they are different.Variables declared using the let keyword can change their values in the future." +
+            "Variables declared using the const keyword cannot change their values in future. This is why you must always" +
+            "initialize the variables declared with the const keyword.remember that variables declared using the const keyword" +
+            "are read-only & cannot be reassigned any value."
     },
     "card_2": {
         "author": "Colby Fayock",
         "subject": "What is linting and how can it save you time?",
-        "description": ""
+        "description": "One of the biggest challenges in software development is time. It’s something we can’t easily get more of," +
+            "but linting can help us make the most out of the time we have.Simply put, a linter is a tool that programmatically scans" +
+            "your code with the goal of finding issues that can lead to bugs or inconsistencies with code health and style. Some can" +
+            "even help fix them for you! To get started, we first need a linter. Probably the most popular in the Javascript world is ESLint." +
+            "Your linter will actually be the engine for defining rules and parsing your files to test against. ESLint is available as an npm" +
+            "package by itself and once installed, out of the box it allows you to set up a basic configuration file and hit the ground running" +
+            "with some command line tools."
     },
     "card_3": {
         "author": "Yazeed Bzadough",
         "subject": "How to Get More Views on Your Tech Blog",
-        "description": ""
+        "description": "If you're a developer with a Twitter account, you've already seen everyone and their cat start a blog, YouTube channel," +
+            "or Patreon. We all want to become stars, or at the very least, a recognizable name in the industry.This is priority #1 before optimizing" +
+            "your blog's layout, SEO, or marketing strategy. Make sure you understand your technology well! If you don't understand it, how can you" +
+            "help others understand it?"
     },
     "card_4": {
         "author": "Cedd Burge",
         "subject": "How to write easily describable code",
-        "description": ""
+        "description": "When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes" +
+            "mental energy,and you run the risk of getting the mapping wrong. Different people will map to different words, which leads to confusion" +
+            "when discussing the code.This is usually a fertile breeding ground for bugs born out of miscommunication / misunderstanding, and fixing" +
+            "these bugs often introduces new ones, for the same reasons. In the end it becomes code that no one really understands or wants to touch."
     },
     "card_5": {
         "author": "Srishti Gupta",
-        "subject": "Everything you should know about ‘module’ & ‘require’ in Node.js",
+        "subject": "Everything you should know about 'module' & 'require' in Node.js",
         "description": "Node.js treats each JavaScript file as a separate module. For instance, if you have a" +
             "file containing some code and this file is named xyz.js, then this file is treated as a module in" +
             "Node, and you can say that you’ve created a module named xyz.Let’s take an example to understand this" +
