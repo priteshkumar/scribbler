75181dce1c736948f2249d23554e8e360cb370ac
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-10-05 21:11:20 +0530

    reset createpost form fields to default onclose

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,11 @@ var postslink = './html/postslist.html';
 function showBlogs(event) {
     window.location.href = postslink;
 }
+
+$('#ModalPost').on("hidden.bs.modal", function (e) {
+    var createPostForm = document.forms["createpostform"];
+    var titleInput = createPostForm.elements['InputTitle'];
+    titleInput.value = titleInput.defaultValue;
+    var contentInput = createPostForm.elements['InputContents'];
+    contentInput.value = contentInput.defaultValue;
+});
