fc3355bb4ea7d1e94cb02e2d572a4dc8b404962e
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-10-05 17:57:30 +0530

    fix dynamic content creation in post page
    use sessionstorage in postslist page, to set postentry
    in post.html page corresponding postentry will be displayed

--- scripts/post.js
+++ scripts/post.js
@@ -1,17 +1,56 @@
-var postList = [
-    {
-        "id": "card_1",
+var postsMap = {
+    "card_1": {
+        "author": "Srishti Gupta",
+        "subject": "‘let’ me be a ‘const’(ant), not a ‘var’(iable)!",
+        "description": ""
+    },
+    "card_2": {
         "author": "Colby Fayock",
-        "subject": "What is linting and how can it save your time"
+        "subject": "What is linting and how can it save you time?",
+        "description": ""
+    },
+    "card_3": {
+        "author": "Yazeed Bzadough",
+        "subject": "How to Get More Views on Your Tech Blog",
+        "description": ""
+    },
+    "card_4": {
+        "author": "Cedd Burge",
+        "subject": "How to write easily describable code",
+        "description": ""
+    },
+    "card_5": {
+        "author": "Srishti Gupta",
+        "subject": "Everything you should know about ‘module’ & ‘require’ in Node.js",
+        "description": "Node.js treats each JavaScript file as a separate module. For instance, if you have a" +
+            "file containing some code and this file is named xyz.js, then this file is treated as a module in" +
+            "Node, and you can say that you’ve created a module named xyz.Let’s take an example to understand this" +
+            "better.You have a file named circle.js which consists of the logic for calculating the area" +
+            "& the circumference of a circle of a given radius, as given below: circle.jsYou can call circle.js file a module named circle.You might" +
+            "be wondering why is there a need to have multiple modules? You could have just written all the code in a" +
+            "single module.Well, it is very important to write modular code.By modular, I mean to say that your code should be" +
+            "independent and should be loosely coupled.Imagine that there’s a large application and you have all your code written" +
+            "in just one place, just one file.Too messy, right?This is the most significant advantage of having modules in Node.js." +
+            "Even if you define a global variable in a module using var, let or const keywords, the variables are scoped locally to the" +
+            "module rather than being scoped globally.This happens because each module has a function wrapper of its own and the code" +
+            "written inside one function is local to that function and cannot be accessed outside this function."
     }
-];
+};
 
 function initUI() {
     var postId = window.sessionStorage.getItem('postId');
     console.log(postId);
+    var postEntry = postsMap[postId];
+    var subject = document.getElementById("subject");
+    subject.innerHTML = '<h5>' + postEntry["subject"] + '</h5>';
+    var author = document.getElementById("author");
+    var authorHtml = author.innerHTML;
+    author.innerHTML = '<p>' + postEntry["author"] + '</p>' + authorHtml;
+    var description = document.getElementById('description');
+    description.innerHTML += '<p>' + postEntry["description"] + '</p>';
 }
 
-//initUI();
+initUI();
 
 function enableEditMode(e, editBtn) {
     console.log(editBtn);
@@ -90,7 +129,7 @@ function updateLikes(event, likeBtn) {
     }
     likeCounter = likeCounter + 1;
     var likeInfo = document.getElementById('like-info');
-    likeBtn.setAttribute("data-likes",String(likeCounter));
-    var likeMsg = likeCounter == 1 ?' person likes this!':' people like this!';
+    likeBtn.setAttribute("data-likes", String(likeCounter));
+    var likeMsg = likeCounter == 1 ? ' person likes this!' : ' people like this!';
     likeInfo.innerText = likeCounter + likeMsg;
 }
